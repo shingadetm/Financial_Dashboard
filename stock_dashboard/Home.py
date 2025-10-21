8702127eae608f93f09f004c1af8f44472a7a1ab
Tushar Shingade <tusharshingade100@gmail.com>
2025-10-22 04:58:51 +0530

    Fix formatting of welcome message in Home.py

--- stock_dashboard/Home.py
+++ stock_dashboard/Home.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
 st.title("Financial Dashboard")
-st.write("Welcome to your analytics hub,
-developing more advanced version, coming soon✨")
+st.write("Welcome to your analytics hub,developing more advanced version, coming soon✨")
