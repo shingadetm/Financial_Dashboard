40a9b7b928990ae2e959343699ee3ba0f34db183
Tushar Shingade <tusharshingade100@gmail.com>
2025-10-22 05:14:54 +0530

    Add navigation link to Live Chart page
    Added a page link to navigate to the Live Chart.

--- stock_dashboard/Home.py
+++ stock_dashboard/Home.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("Financial Dashboard")
 st.write("Welcome to your analytics hub,developing more advanced version, coming soon✨")
+## use of page link
+st.page_link("pages/01_LiveChart.py", label="Go to Live Chart")
